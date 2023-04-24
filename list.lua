70212550890afa3d0ac94405ff7637190070822a
paripal <86734575+paripalpp@users.noreply.github.com>
2023-04-25 00:47:01 +0900

    Update list.lua

--- list.lua
+++ list.lua
@@ -42,6 +42,8 @@ function update_readme()
         "%1"..scan_sym().."%2")
     file = file:gsub("(<!%-%- start_footprint_list %-%->\n).-(<!%-%- end_footprint_list %-%->)",
         "%1"..scan_fp().."%2")
+    file = file:gsub("(<!-- start_timestamp -->\n).-(<!-- end_timestamp -->)",
+        "%1"..os.date( "!%a %b %d %H:%M:%S %Y UTC\n").."%2")
     io.open("./README.md", "w+"):write(file)
 end
 
